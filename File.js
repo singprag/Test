d169c6c23b0660cba590090f74d58f782ebfce26
singprag <47734658+singprag@users.noreply.github.com>
2019-02-18 12:21:13 +0530

    Create File.js

---
+++ File.js
@@ -0,0 +1,11 @@
+<script>
+
+var init = "Hello"
+var nameArray = ["Pragya", "Arushi", "Sam", "Lilly"];
+var len = nameArray.length();
+for(var i = 0; i < len; i++)
+{
+  log.console(init+" "+nameArray[i]_;
+}
+
+</script>
